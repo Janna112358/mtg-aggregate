567d5a2592ff82f31f08ca9a97a86466b00e49cd
Janna Goldstein <jgoldstein@star.bham.ac.uk>
2018-09-12 17:11:01 +0100

    add some docstrings

--- mtg-aggregate/aggregate.py
+++ mtg-aggregate/aggregate.py
@@ -21,13 +21,16 @@ def create_aggregate(decks, save=None, num_main=60, num_side=15, symbol='#'):
     
     Returns
     -------
-    Deck: the aggregate decklist
+    main: dict
+        dictionary of aggregate maindeck
+    side: dict
+        dictionary of aggregate sideboard
     """
-    count_all_cards_main = count_aggregate_cards(decks, main=True, symbol=symbol)
-    count_all_cards_side = count_aggregate_cards(decks, main=False, symbol=symbol)
+    count_all_cards_main = _count_aggregate_cards(decks, main=True, symbol=symbol)
+    count_all_cards_side = _count_aggregate_cards(decks, main=False, symbol=symbol)
     max_occurence = len(decks)
-    agg_main = make_deck_from_count(count_all_cards_main, max_occurence, num_main)
-    agg_side = make_deck_from_count(count_all_cards_side, max_occurence, num_side)
+    agg_main = _make_deck_from_count(count_all_cards_main, max_occurence, num_main)
+    agg_side = _make_deck_from_count(count_all_cards_side, max_occurence, num_side)
 
     main = {}
     for agg_card in agg_main:
@@ -47,7 +50,32 @@ def create_aggregate(decks, save=None, num_main=60, num_side=15, symbol='#'):
     
     return main, side  
 
-def count_aggregate_cards(decks, main=True, symbol='#'):
+def _count_aggregate_cards(decks, main=True, symbol='#'):
+    """
+    Count aggregate occurence of cards in a list of decks
+    
+    Helper function for create_aggregate. Can also be used to get a ranking of
+     the most played cards in the given list of decks
+    
+    Parameters
+    ---------
+    decks: list
+        list of Deck objects
+    main: bool
+        default = True
+        if True, count maindeck aggregate
+        if False, count sideboard aggregate
+    symbol: str
+        default = '#'
+        symbol to use in numbering of aggregate cards (i.e. the difference between
+        'Negate #1' and 'Negate #2'). Use something that does not occur in card names!
+        
+    Returns
+    -------
+    dict:
+        counts of all aggregate cards (i.e. making the distinction between 
+        'Negate #1' and 'Negate #2')
+    """
     count_cards = {}
     for deck in decks:
         if main:
@@ -64,7 +92,29 @@ def count_aggregate_cards(decks, main=True, symbol='#'):
                     count_cards[agg_card] += 1
     return count_cards
 
-def make_deck_from_count(count_cards, max_occurence, len_decklist):
+def _make_deck_from_count(count_cards, max_occurence, len_decklist):
+    """
+    Get the normal-named decklist from the aggregate count
+    
+    Helper function for create_aggregate. Used the ranking from the aggregate count
+    to select the best len_decklist number of cards (picking at random where neseccary).
+    
+    Parameters
+    ----------
+    count_cards: dict
+        aggregate card count as returned by _count_aggregate_cards
+    max_occurence: int
+        maximum occurence of any card to assume, should be the number of decks
+        used to make the aggregate count
+    len_decklist: int
+        lenght of the required list (usually 60 for maindeck, 15 for sideboard)
+        
+    Returns
+    -------
+    list
+        list of len_decklist selected aggregate cards (i.e. making the 
+        distinction between 'Negate #1' and 'Negate #2')
+    """
     agg_deck = []
     while len(agg_deck) < len_decklist:
         space_in_deck = len_decklist - len(agg_deck)
